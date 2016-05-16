3676f5a99b43be6e074000691cf52f5c2f38d8b4
ales2003 <ales_2003@mail.ru>
2016-05-16 19:08:57 +0300

    Initial commit

---
+++ src/mista1984/Second.java
@@ -0,0 +1,13 @@
+package mista1984;
+
+/**
+ * Created by USER on 11.05.2016.
+ */
+public class Second {
+    public static void main(String[] args) {
+        for (int i = 2; i >0 ; i=i-5) {
+            System.out.println(i);
+            
+        }
+    }
+}
